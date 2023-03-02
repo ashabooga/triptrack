ebbfb4973c7a9e59adc18ee7513c32eec94bbda1
Natalia <sgnwitko@liverpool.ac.uk>
2023-03-02 15:25:13 +0000

    hi

--- AppPrototype/ViewController.swift
+++ AppPrototype/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         print("hi1")
         print("hello")
         print ("testing ellohhhhhhh")
+        print("twest")
         // Do any additional setup after loading the view.
     }
 
