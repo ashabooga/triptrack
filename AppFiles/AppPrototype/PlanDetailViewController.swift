9e4e37c8053e2409c2141ccf5989ca618c736f67
Ben <bcuellowolffe@gmail.com>
2023-03-10 13:51:59 +0000

    Added for plan VC: table & cells, detail view controller, segues to and back for detail view controller, and nav bar.

---
+++ AppFiles/AppPrototype/PlanDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PlanDetailViewController.swift
+//  AppPrototype
+//
+//  Created by Cuello-Wolffe, Benjamin on 10/03/2023.
+//
+
+import UIKit
+
+class PlanDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+//    
+//    @IBAction func unwindToPlan(_ unwindSegue: UIStoryboardSegue) {
+//        _ = unwindSegue.source
+//        
+//        performSegue(withIdentifier: "toPlan", sender: nil)
+//        // Use data from the view controller which initiated the unwind segue
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AppFiles/AppPrototype/PlanViewController.swift
+++ AppFiles/AppPrototype/PlanViewController.swift
@@ -1,9 +1,50 @@
 import UIKit
 
-class PlanViewController: UIViewController {
+class PlanViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var test = ["plan1", "plan2", "plan3"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "planCell", for: indexPath)
+        var content = UIListContentConfiguration.cell()
+        content.text = test[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toPlanDetail", sender: nil)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            test.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    @IBAction func unwindToPlan(_ unwindSegue: UIStoryboardSegue) {
+        _ = unwindSegue.source
+        
+        
+        // Use data from the view controller which initiated the unwind segue
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
     
 }
