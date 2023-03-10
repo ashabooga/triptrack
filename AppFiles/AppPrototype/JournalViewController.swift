6525fc3c876844fbbb6d796127fca0a28c2bde31
Natalia <sgnwitko@liverpool.ac.uk>
2023-03-10 12:47:04 +0000

    added a TableView to the journal tab that segues to a detailViewController

--- AppFiles/AppPrototype/JournalViewController.swift
+++ AppFiles/AppPrototype/JournalViewController.swift
@@ -1,9 +1,44 @@
 import UIKit
 
-class JournalViewController: UIViewController {
+class JournalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var test = ["hi", "hello", "testing"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+        var content = UIListContentConfiguration.cell()
+        content.text = test[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toJournalDetail", sender: nil)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            test.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    
     
 }
