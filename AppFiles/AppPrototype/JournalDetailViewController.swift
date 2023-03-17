8290992586c2444a50f34c514328a49096185503
Ben <bcuellowolffe@gmail.com>
2023-03-17 14:12:24 +0000

    test commit

--- AppFiles/AppPrototype/JournalDetailViewController.swift
+++ AppFiles/AppPrototype/JournalDetailViewController.swift
@@ -22,6 +22,7 @@ class JournalDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
