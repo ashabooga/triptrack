7c1d322dd3b85207aaacdf12d83c38ee565f21da
Jason Lika <JasonLika@hotmail.com>
2023-03-17 15:50:46 +0000

    Further progress on core location

--- AppFiles/AppPrototype/MapViewController.swift
+++ AppFiles/AppPrototype/MapViewController.swift
@@ -1,30 +1,46 @@
-import UIKit
 import CoreLocation
+import UIKit
+import MapKit
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let cManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        cManager.requestWhenInUseAuthorization()
+    }
+    
+    //4.51
+    
+    
+    
+    
+     
+    /*
     let locationManager = CLLocationManager()
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
         print("locations = \(locValue.latitude) \(locValue.longitude)")
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Ask for Authorisation from the User.
-        self.locationManager.requestAlwaysAuthorization()
-
-        // For use in foreground
-        self.locationManager.requestWhenInUseAuthorization()
-
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-        }
-        
-        
+    if CLLocationManager.locationServicesEnabled() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
     }
-    
+     // Ask for Authorisation from the User.
+     self.locationManager.requestAlwaysAuthorization()
+
+     // For use in foreground
+     self.locationManager.requestWhenInUseAuthorization()
+     import CoreLocation
+
+    */
 }
