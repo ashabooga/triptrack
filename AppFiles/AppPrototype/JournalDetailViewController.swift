dfcfb0354b1d5824c15fca55acc174443cedc993
Natalia <witkowska.natalka@gmail.com>
2023-03-20 14:46:46 +0000

    Added scroll view to JournalDetailVC with images selected from JournalEntryVC

--- AppFiles/AppPrototype/JournalDetailViewController.swift
+++ AppFiles/AppPrototype/JournalDetailViewController.swift
@@ -7,19 +7,30 @@
 
 import UIKit
 
-class JournalDetailViewController: UIViewController {
+class JournalDetailViewController: UIViewController, UIScrollViewDelegate  {
     
     var selectedEntry = ["ID" : Int(), "title" : String(), "location" : String(), "date" : Date(), "textEntry" : String(), "photos" : [UIImage]()] as [String : Any]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
 
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var textEntry: UITextView!
-    @IBOutlet weak var photoView: UIImageView!
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
     
     let dateFormatter = DateFormatter()
     var photoList = [UIImage]()
+    
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var page = scrollView.contentOffset.x/scrollView.frame.size.width
+        pageControl.currentPage = Int(page)
+    }
     
     
     override func viewDidLoad() {
@@ -32,7 +43,21 @@ class JournalDetailViewController: UIViewController {
         dateLabel.text = StringDate
         textEntry.text = selectedEntry["textEntry"] as? String
         photoList = (selectedEntry["photos"] as? [UIImage])!
-        photoView.image = photoList[0]
+        //photoView.image = photoList[0]
+        
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+                
+        for index in 0..<photoList.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            let imageView = UIImageView(frame: frame)
+            imageView.image = photoList[index]
+            self.scrollView.addSubview(imageView)
+        }
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(photoList.count), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+        print(photoList)
         
 
         // Do any additional setup after loading the view.
