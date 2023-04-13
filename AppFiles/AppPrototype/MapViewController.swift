847335b65e295cc075c724e3a11c9a2d2553f6ed
Jason Lika <sgjlika@liverpool.ac.uk>
2023-04-13 22:46:54 +0100

    yeah

--- AppFiles/AppPrototype/MapViewController.swift
+++ AppFiles/AppPrototype/MapViewController.swift
@@ -75,35 +75,4 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         //configure the map to show the user's location (with a blue dot)
         mapView.showsUserLocation = true
     }
-    
-    
-    
-    
-    //let cManager = CLLocationManager()
-    /*override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        cManager.requestWhenInUseAuthorization()
-    }
-    */
-    //4.51
-     
-    /*
-    let locationManager = CLLocationManager()
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
-    }
-    if CLLocationManager.locationServicesEnabled() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.startUpdatingLocation()
-    }
-     // Ask for Authorisation from the User.
-     self.locationManager.requestAlwaysAuthorization()
-
-     // For use in foreground
-     self.locationManager.requestWhenInUseAuthorization()
-     import CoreLocation
-
-    */
 }
