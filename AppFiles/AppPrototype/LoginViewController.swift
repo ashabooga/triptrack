3d9f64453505e7c7672f4c7bf96576b1e6260665
Ben Cuello-Wolffe <bcuellowolffe@gmail.com>
2023-05-04 15:04:00 +0100

    Added Face ID Biometrics

--- AppFiles/AppPrototype/LoginViewController.swift
+++ AppFiles/AppPrototype/LoginViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Cuello-Wolffe, Benjamin on 27/04/2023.
 //
 
+import LocalAuthentication
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -15,7 +16,40 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func FaceIDButton(_ sender: Any) {
+        let context = LAContext()
+        var error: NSError? = nil
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authorize with biometrics"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {[weak self] success, error in
+                
+                DispatchQueue.main.async {
+                    guard success, error ==  nil else {
+                        // Failed
+                        let alert = UIAlertController(title: "Failed to Authenticate", message: "Please try again or use your password.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+                        self!.present(alert, animated: true)
+                        
+                        return
+                    }
+                    
+                    // Success
+                    self!.LoggedIn()
+                }
+            }
+        } else { // Could not use biometrics
+            let alert = UIAlertController(title: "Unavailable", message: "You can't use this feature", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            present(alert, animated: true)
+        }
+    }
+    
+    
+    func LoggedIn() {
+        performSegue(withIdentifier: "loginToWelcome", sender: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
